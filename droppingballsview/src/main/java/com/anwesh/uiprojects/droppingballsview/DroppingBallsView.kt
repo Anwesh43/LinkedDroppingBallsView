ef0e83d348215aaa437d686ba123209f66424089
Anwesh43 <anweshthecool0@gmail.com>
2019-08-09 16:05:17 +0530

    adding canvas extension methods

--- droppingballsview/src/main/java/com/anwesh/uiprojects/droppingballsview/DroppingBallsView.kt
+++ droppingballsview/src/main/java/com/anwesh/uiprojects/droppingballsview/DroppingBallsView.kt
@@ -17,7 +17,7 @@ val balls : Int = 5
 val hFactor : Float = 3f
 val scGap : Float = 0.1f
 val strokeFactor : Int = 90
-val sizeFactor : Float = 2.9f
+val sizeFactor : Float = 2f
 val foreColor : Int = Color.parseColor("#3F51B5")
 val backColor : Int = Color.parseColor("#BDBDBD")
 
@@ -25,3 +25,24 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int)  : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.updateValue(dir : Float) : Float = dir * scGap / balls
+
+fun Canvas.drawBalls(size : Float, xGap : Float, h : Float, hDest : Float, scale : Float, paint : Paint) {
+    for (j in 0..(balls - 1)) {
+        val scj : Float = scale.divideScale(j, balls)
+        drawCircle(j * xGap + size, h + (hDest - h) * scj, size, paint)
+    }
+}
+
+fun Canvas.drawDBNode(i  : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = (h / hFactor) / (nodes)
+    val size : Float = gap / sizeFactor
+    val xGap : Float = w / (2 * size)
+    val destH : Float = h - (i + 1) * gap
+    paint.color = foreColor
+    save()
+    drawBalls(size, xGap, h, destH, scale, paint)
+    restore()
+}
+
