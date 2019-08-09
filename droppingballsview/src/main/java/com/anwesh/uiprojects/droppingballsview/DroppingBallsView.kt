f0d078cfbe8fbf29643376c8792e9813eccb5112
Anwesh43 <anweshthecool0@gmail.com>
2019-08-09 15:52:21 +0530

    adding Int and Float extension methods

--- droppingballsview/src/main/java/com/anwesh/uiprojects/droppingballsview/DroppingBallsView.kt
+++ droppingballsview/src/main/java/com/anwesh/uiprojects/droppingballsview/DroppingBallsView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.droppingballsview
 /**
  * Created by anweshmishra on 09/08/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.content.Context
+import android.app.Activity
+import android.graphics.Color
+
+val nodes : Int = 5
+val balls : Int = 5
+val hFactor : Float = 3f
+val scGap : Float = 0.1f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
