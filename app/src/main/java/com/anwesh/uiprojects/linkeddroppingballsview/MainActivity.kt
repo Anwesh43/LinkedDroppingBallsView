b662de3e2f4c03831df3b3ba73f63b9fc3ad5688
Anwesh43 <anweshthecool0@gmail.com>
2019-08-09 18:10:18 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkeddroppingballsview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkeddroppingballsview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkeddroppingballsview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.droppingballsview.DroppingBallsView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DroppingBallsView.create(this)
     }
 }
